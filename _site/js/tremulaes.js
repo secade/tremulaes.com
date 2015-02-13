fbc288801c4ddc6d2f4f72bfc96370804af982fe
Chris Singer <ccsinger@gmail.com>
2015-02-13 00:49:00 -0500

    Actually add tracked files correctly this time.

--- _site/js/tremulaes.js
+++ _site/js/tremulaes.js
@@ -1,6 +1,4 @@
-window.onload = function() {
-  
-  $(document).ready(function() {
+$(document).ready(function() {
     var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
     $('#js-centered-navigation-menu').removeClass("show");
     
@@ -12,7 +10,24 @@ window.onload = function() {
         }
       });
     });
-  });
-  
-};
+});
 
+$(document).ready(function () {
+  $('.accordion-tabs').each(function(index) {
+    $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+  });
+
+  $('.accordion-tabs').on('click', 'li > a', function(event) {
+    if (!$(this).hasClass('is-active')) {
+      event.preventDefault();
+      var accordionTabs = $(this).closest('.accordion-tabs');
+      accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+      $(this).next().toggleClass('is-open').toggle();
+      accordionTabs.find('.is-active').removeClass('is-active');
+      $(this).addClass('is-active');
+    } else {
+      event.preventDefault();
+    }
+  });
+});
